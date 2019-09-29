34e9fc1d2f7e626e9b5ae58dee457130b7c2921e
nrdeering <nrdeering@gmail.com>
2019-09-28 20:58:37 -0400

    js final

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,31 @@
-// from data.js
-var tableData = data;
 
-// YOUR CODE HERE!
+var tableData = data;
+console.log(data);
+
+var tbody = d3.select("tbody");
+
+data.forEach(function(filteredData) {
+  var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+var inputElement = d3.select("#datetime");
+var inputValue = inputElement.property("value");
+console.log(inputValue);
+console.log(tableData);
+
+var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+console.log(filteredData);
+});
+  var row = tbody.append("tr");
+  Object.entries(filteredData).forEach(function([key, value]) {
+   console.log(key, value);
+   var cell = row.append("td");
+   cell.text(value);
+ });
+});
+
+
+
+
+
+
